a77f881b57d99474d1e9ae03a32f705cbbc1a5b5
Pavel <pavel@zby.cz>
2010-10-29 10:08:43 +0200

    ne plně funkční (staticLink, dataLink)

--- etc/script.js
+++ etc/script.js
@@ -1,4 +1,3 @@
-
 var map_init = function(){
 	var map = new OpenLayers.Map('map');
 	
@@ -87,51 +86,225 @@ $(function(){
 
 
 
-
-
-
-
-
-
-
-var OSMCZ = { //static
-	activePanel: false,
-	panels: {}, //assoc by htmlId
+//--------------------------------- Panel common prototype -------------------
+var Panel = {
+	id: false,
 	
-	addPanel: function(p){OSMCZ.panels[p.htmlId] = p},
 	
-	initStaticPanels: function(){ //add from initial DOM
-		$('#js-panelsContainer').children().each(function(){
-			id = $(this).attr('id');
-			p = new Panel(id);
-			p.setTitle( $(this).attr('title') );
-			            $(this).removeAttr('title');
-		});
+	/** activate THIS panel
+	 */	
+	activate: function(){
+		if(!(OSMCZ.activePanel instanceof StaticPanel)){	//předchozí není static, uložíme
+			OSMCZ.lastActivePanel = OSMCZ.activePanel
+		}
+		OSMCZ.activePanel.hide()
+		this.show();
+		OSMCZ.activePanel = this;
+		$('#tabchooser').text(this.getTitle()).removeClass().addClass('top_'+this.htmlId);
+	},
+	
+	
+	/** show/hide this panel, toggle active flag
+	 */	
+	hide: function(){$('#'+this.id).hide()},
+	show: function(){$('#'+this.id).show()},
+	
+	
+	/** setId() - init html div
+	 */
+	setId: function(id){	
+		if(!id)
+			id = 'pnl'+Math.round(Math.random()*100000); //random name?
+			
+		if(!document.getElementById(id)) //create new html panel?
+			$('#js-panelsContainer').append('<div id="'+id+'" class="panel hidden"></div>');
 		
-		new Summary();
+		this.id = id;
+	},
+	
+	
+	/** setTitle(), getTitle()
+	 */	
+	setTitle: function (str){this.title = str;},
+	getTitle: function (){return this.title;}
+	
+}
+
+//--------------------------------- StaticPanel ----------------------------------
+var StaticPanel = function(){}
+StaticPanel.prototype = $.extend({}, Panel, {
+	Panel: Panel
+});
+
+
+//--------------------------------- DataPanel ----------------------------------
+var DataPanel = function(){}
+DataPanel.prototype = $.extend({}, Panel, {
+	Panel: Panel	
+});
+
+
+
+
+
+
+
+
+
+
+var OSMCZ = {
+	lastActivePanel: null,
+	activePanel: null,
+	panelsById: {}, 				//assoc by htmlId
+	panelsByQuery: {},
+	statics: {},
+	
+	
+	/** initialization() of UI
+	 */	
+	init: function(){ 
+		OSMCZ.statics.summary = new Summary();
+		// OSMCZ.statics.routingForm = new RoutingForm();
+		// OSMCZ.statics.upload = new Upload();
+		// OSMCZ.statics.export = new Export();
+		// OSMCZ.statics.print = new Print();
+		// OSMCZ.statics.export = new Export();
+		$('.staticLink').bind('click', function(e){alert('xasgas'+this.href);OSMCZ.statics[this.href.substr(1)].activate();return false;});
 		
-		OSMCZ.panels.summary.add(OSMCZ.panels['home']);
-		OSMCZ.activePanel = OSMCZ.panels['home'];
+		
+		OSMCZ.handleQuery(''); 
+		$('.dataLink').live('click', function(e){alert('xasgas'+this.href);OSMCZ.handleQuery(this.href.substr(1));return false;});
+		//window.location.hash.substr(1) .. možnos sloučit na link
+	},
+	
+	
+	/** tabchooser() Clicked
+	 */
+	tabchooser: function(){
+		if(OSMCZ.activePanel instanceof StaticPanel)	//funkce křížku
+			OSMCZ.lastActivePanel.activate();
+		else 																					//funkce "zobraz Summary"
+			OSMCZ.statics.summary.activate();
+	},
+	
+	
+	/** handleQuery() - simple string
+	 */	
+	dataPanels: [Home, Routing], //Routing,OsmData,Coords,Address,BBox,MapUrl
+	handleQuery: function (query){
+		var matchedObject = false;
+		
+		//load from cache
+		if(OSMCZ.panelsByQuery[query]){
+			matchedObject = OSMCZ.panelsByQuery[query];
+			return;
+		}
+
+		//find module matching the query
+		if(!matchedObject){
+			for (i in OSMCZ.dataPanels){
+				if(data = OSMCZ.dataPanels[i].parseQuery(query)){
+					matchedObject = new OSMCZ.dataPanels[i](); //setTitle, setId
+					matchedObject.setData(data);
+					matchedObject.setQuery(query);
+					break;
+				}
+			}
+		}
+		
+		//přidat do Summary,panels,panelsByQuery
+		if(matchedObject){
+			var p = matchedObject;
+			OSMCZ.statics.summary.add(p);
+			OSMCZ.panelsById[p.id] = p;
+			OSMCZ.panelsByQuery[p.query] = p;
+			p.activate();
+		}
+		else {
+			alert('Nic nenalezeno');
+		}
 	}
 }
-$(OSMCZ.initStaticPanels);
+$(OSMCZ.init);
 
 
 
-//--------------------------------- Panel ----------------------------------
+//--------------------------------- SUMMARY : StaticPanel ------------------------
+var Summary = function(){
+	//není potřeba: this.Panel();
+	this.setTitle("Přehled panelů");
+	this.setId('summary');
+	
+	// 	$("#summary .item a").live("click", function(event){
+	// 		alert($(this).parent().attr('id'));
+	// 		return false;
+	// 	});
+	// 	
+	// 	$("#summary .item input").live("click", function(event){
+	// 		id = $(this).parent().parent().attr('id');
+	// 		OSMCZ.panels[id].toggleDataLayer();
+	// 		return false;
+	// 	});
+
+}
+Summary.prototype = new StaticPanel();
+Summary.prototype.add = function(p){
+	$('#summary').append('<div class="item" id="item_'+p.id+'"><span><input type="checkbox" name="a"> <a href="#">X</a></span><a href="#'+p.getQuery()+'">'+p.getTitle()+'</a></div>');
+}
+Summary.prototype.remove = function(p){
+	$('#item_'+p.id).remove();
+}
+
+//--------------------------------- Home : DataPanel ------------------------
+var Home = function(){
+	//není potřeba: this.Panel();
+	this.setTitle("Vítejte");
+	this.setId('home');
+}
+Home.prototype = new StaticPanel();
+Home.parseQuery = function (query){return (query=='');}
+
+//--------------------------------- Routing : DataPanel ------------------------
+var RoutingForm = function(){
+	//není potřeba: this.Panel();
+	this.setTitle("Plánování trasy");
+	this.setId('routingform');
+}
+Routing.prototype = new StaticPanel();
+Routing.parseQuery = function (query){return (query=='');}
+
+
+//--------------------------------- ROUTING : Panel ------------------------
+var Routing = function(){
+	//není potřeba: this.Panel();
+	//OSMCZ.summary.add(this);
+}
+Routing.prototype = new Panel(); 
+Routing.prototype.createForm = function (){}
+Routing.prototype.findRoute = function (){}
+Routing.parseQuery = function (query){ //static
+		arr = query.split('>');
+		if(!arr[1]) return false;
+		return {from: arr[0], to: arr[1]};
+}
+
+
+
+
+
+//--------------------------------- Dtsdfadfasasadfnel ----------------------------------
+//--------------------------------- DataPanel ----------------------------------
+//--------------------------------- DataPanel ----------------------------------
+//--------------------------------- DataPanel ----------------------------------
+//--------------------------------- DataPanel ----------------------------------
+
+
+
+
+
+
+
 var Panel = function(id){
-	//initHtml
-	if(!id)
-		id = 'pnl'+Math.round(Math.random()*100000); //random name?
-		
-	if(!document.getElementById(id)){ //create new html panel?
-		$('#js-panelsContainer').append('<div id="'+id+'" class="panel hidden"></div>');
-	}
-	
-	this.htmlId = id;
-	
-	//add to main register
-	OSMCZ.addPanel(this);
 }
 Panel.prototype = {
 	title: "",
@@ -140,27 +313,11 @@ Panel.prototype = {
 	active: false,
 	Panel: Panel, //constructor
 
-	setTitle: function (str){this.title = str; if(this.active) $('#tabchooser').text(str)},
-	getTitle: function (){return this.title;},
+
 	setQuery: function (){},
 	getQuery: function (){},
 	parseQuery: function (query){},
 	constructQuery: function (){},
-	
-	showInSumm: false,
-
-	
-	//** show/hide this panel, toggle active flag
-	hide: function(){$('#'+this.htmlId).hide(); this.active = false;},
-	show: function(){$('#'+this.htmlId).show(); this.active = true;},
-	
-	//** activate this panel - hide previous, show this and set "active" flags
-	activate: function(){
-		OSMCZ.activePanel.hide()
-		OSMCZ.activePanel = this;
-		this.show();
-		$('#tabchooser').text(this.getTitle()).removeClass();
-	},
 	
 	addHtml: function (){},
 	addFeatures: function (){},
@@ -169,45 +326,7 @@ Panel.prototype = {
 }
 
 
-//--------------------------------- SUMMARY : Panel ------------------------
-var Summary = function(){
-	this.Panel('summary');
-	this.setTitle("Přehled panelů");
-	
-	$("#summary .item a").live("click", function(event){
-		alert($(this).parent().attr('id'));
-		return false;
-	});
-	
-	$("#summary .item input").live("click", function(event){
-		id = $(this).parent().parent().attr('id');
-		OSMCZ.panels[id].toggleDataLayer();
-		return false;
-	});
 
-}
-Summary.prototype = new Panel();
-Summary.prototype.add = function(p){
-	$('#summary').append('<div class="item" id="item_'+p.htmlId+'"><span><input type="checkbox" name="a"> <a href="#">X</a></span><a href="#'+p.getQuery()+'">'+p.getTitle()+'</a></div>');
-}
-Summary.prototype.activate = function(){
-	if(OSMCZ.activePanel != this){
-		this.prevPanel = OSMCZ.activePanel;
-		OSMCZ.activePanel.hide()
-		OSMCZ.activePanel = this;
-		this.show();
-		$('#tabchooser').text(this.getTitle()).removeClass().addClass('top_'+this.htmlId);
-	}
-	else {
-		this.prevPanel.activate();
-	}
-}
-
-Summary.prototype.remove = function(p){
-	//OSMCZ.panels['home'].activate(); //zobraz úvod
-	OSMCZ.panels[p.htmlId] = false; //delete from this.panels
-	//delete from this.summary.DOM
-}
 
 
 
@@ -234,48 +353,8 @@ function xxx(data){
 
 
 
-/// handle Query
-queryModules = [Routing];
-function handleQuery(){
-	query = $('search-autocomplete').value;
-	
-	//find module matching the query
-	queryObject = false;
-	for (i in queryModules){
-		if(data = queryModules[i].parseQuery(query)){
-			queryObject = new queryModules[i](data);
-			break;
-		}
-	}
-	
-	//module matched, add panel
-	if(queryObject){
-		panels.addNew(queryObject);
-		panels.setActive(queryObject);
-	}
-	else {
-		alert('Nic nenalezeno')
-	}
-}
 
 
 
 
-
-
-//--------------------------------- ROUTING : Panel ------------------------
-var Routing = function(data){
-	this.Panel = Panel;
-	this.Panel();
-	this.data=data;
-	OSMCZ.summary.add(this);
-}
-Routing.prototype = new Panel(); 
-Routing.prototype.createForm = function (){}
-Routing.prototype.findRoute = function (){a}
-Routing.parseQuery = function (query){ //static
-		arr = query.split('>');
-		if(!arr[1]) return false;
-		return {from: arr[0], to: arr[1]};
-}
 
